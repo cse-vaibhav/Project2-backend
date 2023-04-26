14f74ff97b43eda6abeb1569dddb836504e7bb22
cse-vaibhav <jainvaibhav671@gmail.com>
2023-04-26 08:30:00 +0530

    Made a search api to filter products

--- recommender_system/app.py
+++ recommender_system/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 import joblib
 import os
-import json
 
 from .recommender import Recommender
 
@@ -14,16 +13,20 @@ print(model_path)
 model: Recommender = joblib.load(model_path)
 
 
+# Return all data
 @app.route("/", methods=["GET", "POST"])
 def index():
     data_with_id = model.data.copy()
     data_with_id["ID"] = data_with_id.index
+    data_with_id = data_with_id.sort_values(by=["ratings_5max"], ascending=False)
+
     if request.method == "GET":
         return f"{data_with_id.to_html()}"
     else:
         return {"data": list(data_with_id.head(10).T.to_dict().values())}
 
 
+# Return 'n' Products
 @app.route("/products/<int:n>", methods=["GET", "POST"])
 def get_products(n: int):
     n_data = model.data.sample(n)
@@ -33,6 +36,7 @@ def get_products(n: int):
         return {"data": list(n_data.drop("clusters", axis=1).T.to_dict().values())}
 
 
+# Return 'n' recommendations for 'id' Product
 @app.route("/recommendations/<int:id>/<int:n>", methods=["GET", "POST"])
 def get_recommendations(id: int, n: int):
     product = model.data.iloc[id, :-1]
@@ -43,3 +47,26 @@ def get_recommendations(id: int, n: int):
         return f"{recommendations.to_html()} <h1>This will return '{n}' recommendations where Product ID: '{id}'</h1>"
     else:
         return {"data": list(recommendations.T.to_dict().values())}
+
+
+# Get Results based on given parameters
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "GET":
+        return str(dict(model.data.dtypes))
+
+    # filter data
+    data = request.json
+    df = model.data.copy()
+
+    for k,v in data.items():
+
+        if df.dtypes[k] == "int64":
+            v = int(v)
+        elif df.dtypes[k] == "float64":
+            v = float(v)
+
+        df = df[df[k] == v]
+    
+    print(df)
+    return list(df.T.to_dict().values())
